79e80ca7d388238dba0bb90f2d2f08b8c1c674e3
Anıl Şenay <anil.senay@cimri.com>
2023-08-07 23:17:28 +0300

    Add kafka basic example

---
+++ 01-kafka/01-basic-example/go.mod
@@ -0,0 +1,5 @@
+module github.com/anilsenay/go-kafka-elastic-k8s/basic-example
+
+go 1.19
+
+require github.com/confluentinc/confluent-kafka-go v1.9.2
---
+++ 01-kafka/01-basic-example/producer/cmd/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/anilsenay/go-kafka-elastic-k8s/basic-example/producer"
+)
+
+var topic = flag.String("topic", "mytopic", "topic name")
+var message = flag.String("message", "HELLO", "message to send")
+var count = flag.Int("count", 1, "message count to send")
+
+func main() {
+	flag.Parse()
+
+	producer, err := producer.NewProducer("localhost:9092", *topic)
+	if err != nil {
+		panic(err)
+	}
+	defer producer.Close()
+
+	fmt.Println(*count)
+	for i := 0; i < *count; i++ {
+		msg := *message + "-" + strconv.Itoa(i)
+		err = producer.Produce(msg)
+		if err != nil {
+			panic(err)
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+}
